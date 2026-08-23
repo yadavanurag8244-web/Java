afcc4411f42e716a5d119a1e667fd8fc82c4f4db
yadavanurag8244-web <yadavanurag8244@gmail.com>
2026-08-23 19:45:32 +0530

    3rd java program

---
+++ Arith.java
@@ -0,0 +1,10 @@
+class Arith{
+public static void main(String args[]){
+int num1=5;
+int num2=3;
+System.out.println("Addition of two number is:"+(num1+num2));
+System.out.println("subtraction of two number is:"+(num1-num2));
+System.out.println("Multiplication of two number is: "+(num1*num2));
+System.out.println("Divison of two number is: "+(num1/num2));
+}
+}
